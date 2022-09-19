048a1b37503f6eee9119efbaf3e42e3841fc2c58
agungzxx <agungiman98@gmail.com>
2022-09-19 16:13:16 +0700

    coba

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (test) => {
+  let counter = 0;
+
+  return promiseTheaterIXX()
+    .then(
+      (resolve) => (counter += resolve.filter((m) => m.hasil === test).length)
+    )
+    .then(() => promiseTheaterVGC())
+    .then(
+      (resolve) => (counter += resolve.filter((m) => m.hasil === test).length)
+    )
+    .then(() => counter)
+    .catch((err) => err.response);
+};
 
 module.exports = {
   promiseOutput,
